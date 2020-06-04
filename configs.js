a04ad3b853586eb6cf9cafd5aa16928a490df02b
JonAnderFernandez <fernandezja00@gmail.com>
2020-06-04 13:14:48 +0200

    Init commit

---
+++ configs.js
@@ -0,0 +1,22 @@
+properties = {
+	userLocationRefresh : 2000, // milliseconds
+
+	notificationTimeout : 10000, // milliseconds
+
+	shortDistance 		: 100,   // meters
+	mediumDistance 		: 200,   // meters
+	longDistance 		: 1000,    // meters
+
+	kmlFiles 			: [
+		'1.kml', 
+		'2.kml', 
+		'3.kml', 
+		'4.kml'
+	],
+
+	icons 				: {
+		user 	: '/img/map.png',
+		info 	: '/img/information.png',
+		warning : '/img/alarm.png'
+	}
+}
